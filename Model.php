26351cec92b4c0a8517e43a1211776431aea91e7
Lucas Magroski <magroski@gmail.com>
2017-07-25 16:32:34 -0300

    Overriding default constructor

--- Model.php
+++ Model.php
@@ -5,45 +5,62 @@ namespace Frogg;
 use Phalcon\Mvc\Model as PhalconModel;
 use Frogg\Crypto\WT;
 
-class Model extends PhalconModel{
+class Model extends PhalconModel
+{
 
-	public function permalinkFor($attribute){
-		$tmp = new Permalink($this->$attribute);
-		return $this->getNumeration($tmp->create());
-	}
+    public function __construct($id = null)
+    {
+        if (!is_null($id) && ($self = static::findFirstById($id))) {
+            return $self;
+        }
+    }
 
-	public function permalinkForValues($values){
-		for ($i = 0; $i < count($values); $i++) {
-			$values[$i] = Permalink::createSlug($values[$i]);
-		}
-		$value = implode('-', $values);
-		return $this->getNumeration($value);
-	}
+    public function permalinkFor($attribute)
+    {
+        $tmp = new Permalink($this->$attribute);
 
-	public function tokenId($key){
-		return WT::encode(['id'=>$this->id],$key);
-	}
+        return $this->getNumeration($tmp->create());
+    }
 
-	public static function getByTokenId($token, $key){
-		$data = WT::decode($token, $key);
-		return isset($data->id) ? static::findFirstById($data->id) : false ;
-	}
+    public function permalinkForValues($values)
+    {
+        for ($i = 0; $i < count($values); $i++) {
+            $values[$i] = Permalink::createSlug($values[$i]);
+        }
+        $value = implode('-', $values);
 
-	private function getNumeration($slug) {
-		$resultset = $this->getReadConnection()->query("SELECT `permalink`
+        return $this->getNumeration($value);
+    }
+
+    public function tokenId($key)
+    {
+        return WT::encode(['id' => $this->id], $key);
+    }
+
+    public static function getByTokenId($token, $key)
+    {
+        $data = WT::decode($token, $key);
+
+        return isset($data->id) ? static::findFirstById($data->id) : false;
+    }
+
+    private function getNumeration($slug)
+    {
+        $resultset = $this->getReadConnection()->query("SELECT `permalink`
 														FROM `".$this->getSource()."`
 														WHERE `permalink` = '$slug'
 														LIMIT 1");
-		$i = 1;
-		$tmp = $slug;
-		while($resultset->numRows()){
-			$slug = $tmp.'-'.$i++;
-			$resultset = $this->getReadConnection()->query("SELECT `permalink`
+        $i         = 1;
+        $tmp       = $slug;
+        while ($resultset->numRows()) {
+            $slug      = $tmp.'-'.$i++;
+            $resultset = $this->getReadConnection()->query("SELECT `permalink`
 															FROM `".$this->getSource()."`
 															WHERE `permalink` = '$slug'
 															LIMIT 1");
-		}
-		return $slug;
-	}
+        }
+
+        return $slug;
+    }
 
 }
