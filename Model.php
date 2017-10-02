6223890058010ce60f86c4ce0c0d4589b33fc488
Alexandre Reis Ribeiro <dreanmer@gmail.com>
2017-10-02 10:08:01 -0300

    Add criteria calling on query method

--- Model.php
+++ Model.php
@@ -3,16 +3,33 @@
 namespace Frogg;
 
 use Frogg\Crypto\WT;
+use Phalcon\Di;
+use Phalcon\DiInterface;
 use Phalcon\Mvc\Model as PhalconModel;
 
-/**
- * Class Model
- * @package Frogg
- *
- * @method static static findFirstById(int $id)
- */
 class Model extends PhalconModel
 {
+    /** @return static */
+    public static function findFirstById($id)
+    {
+        return parent::findFirstById($id);
+    }
+
+    /**
+     * @param DiInterface|null $dependencyInjector
+     *
+     * @return \Frogg\Criteria $criteria
+     */
+    public static function query(DiInterface $dependencyInjector = null)
+    {
+        $class = '\\'.get_called_class().'Criteria';
+        /** @var \Frogg\Criteria $criteria */
+        $criteria = new $class();
+        $criteria->setDI($dependencyInjector ?? Di::getDefault());
+        $criteria->setModelName(get_called_class());
+
+        return $criteria;
+    }
 
     public function columnMap()
     {
@@ -30,24 +47,14 @@ class Model extends PhalconModel
         return $columnMap;
     }
 
-    /**
-     * @param string $attribute Name of the attribute that will be used to create a permalink
-     *
-     * @return string A permalink formatted string
-     */
-    public function permalinkFor(string $attribute): string
+    public function permalinkFor($attribute)
     {
         $tmp = new Permalink($this->$attribute);
 
         return $this->getNumeration($tmp->create());
     }
 
-    /**
-     * @param array $values Values that will be used to create a permalink
-     *
-     * @return string A permalink formatted string
-     */
-    public function permalinkForValues(array $values): string
+    public function permalinkForValues($values)
     {
         for ($i = 0; $i < count($values); $i++) {
             $values[$i] = Permalink::createSlug($values[$i]);
@@ -57,7 +64,7 @@ class Model extends PhalconModel
         return $this->getNumeration($value);
     }
 
-    public function tokenId($key): string
+    public function tokenId($key)
     {
         return WT::encode(['id' => $this->id], $key);
     }
@@ -69,7 +76,7 @@ class Model extends PhalconModel
         return isset($data->id) ? static::findFirstById($data->id) : false;
     }
 
-    private function getNumeration($slug): string
+    private function getNumeration($slug)
     {
         $resultset = $this->getReadConnection()->query("SELECT `permalink`
 														FROM `".$this->getSource()."`
@@ -89,3 +96,4 @@ class Model extends PhalconModel
     }
 
 }
+
