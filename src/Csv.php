863801bc451070c92e7be36e7361b53d60ceb56d
Alexandre Reis <dreanmer@gmail.com>
2017-12-08 11:22:01 -0200

    change csv lib

--- src/Csv.php
+++ src/Csv.php
@@ -2,30 +2,30 @@
 
 namespace Frogg;
 
+use League\Csv\Writer;
+
 class Csv
 {
 
+    /**
+     * @param        $list
+     * @param string $filename
+     * @param string $separator
+     * @param string $enclosure
+     *
+     * @throws \TypeError
+     */
     static function export($list, $filename = 'data', $separator = ',', $enclosure = '"')
     {
         header('Content-Type: text/csv; charset=utf-8');
         header('Content-Disposition: attachment; filename='.$filename.'.csv');
 
-        // create a file pointer connected to the output stream
-        $output = fopen('php://output', 'w');
-
-        foreach ($list as $line) {
-            fputcsv($output, $line, $separator, $enclosure);
-        }
+        $writer = Writer::createFromPath('php://output', 'w');
+        $writer->insertAll($list); //using an array
     }
 
     static function addVal(&$line, $value)
     {
-        if (is_int($value)) {
-            $line[] = '='.utf8_decode($value);
-
-            return;
-        }
-        $line[] = '="'.utf8_decode($value).'"';
+        $line[] = utf8_decode($value);
     }
-
 }
