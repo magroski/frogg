b1335ce5b1fed17927f0e9d2da70f850888028ee
magroski <magroski@gmail.com>
2017-12-12 08:55:11 -0200

    Docs

--- src/Model/EmptyResultSet.php
+++ src/Model/EmptyResultSet.php
@@ -10,6 +10,12 @@ namespace Frogg\Model;
 
 use Frogg\Model;
 
+/**
+ * Class EmptyResultSet
+ *       This class is to be used as the empty return on functions that are expected
+ *       to return empty ResultSet but return and array instead.
+ * @package Frogg\Model
+ */
 class EmptyResultSet extends ResultSet
 {
 
