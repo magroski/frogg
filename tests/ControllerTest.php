0fe95683829223749796f3ce4e7b4fbc51bf33f4
Joilson Rezende <joilson560@gmail.com>
2018-01-15 10:45:09 -0200

    Complete test, Phalcon installed on CircleCi

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -7,15 +7,16 @@ class ControllerTest extends TestCase
     public function testGetDefaultParam()
     {
         $controller   = new \Frogg\Controller();
-        $request      = null;
+        $dispatcher   = new \Phalcon\Mvc\Dispatcher();
+        $request      = new Phalcon\Http\Request();
         $defaultValue = 'Default Value';
         $nameGET      = "Test Get";
         $_GET['name'] = $nameGET;
 
-        $controller->request = $request;
+        $controller->request    = $request;
+        $controller->dispatcher = $dispatcher;
 
-        $this->markTestIncomplete(
-            'This test has not been implemented, not possible inject [Request]. Phalcon not installed for circle CI'
-        );
+        $this->assertEquals($nameGET, $controller->getParam('name', $defaultValue));
+        $this->assertEquals($defaultValue, $controller->getParam('color', $defaultValue));
     }
 }
